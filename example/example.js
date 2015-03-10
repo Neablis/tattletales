74f41afa0a7e682084f4c3ae078987a229841903
Mitchell <Neablis121@gmail.com>
2015-03-09 22:40:32 -0700

    Using advice from a friend, got much easier to parse git logs and now have email along with name

--- example/example.js
+++ example/example.js
@@ -1,10 +1,13 @@
 var gitblame = require('../tattletales');
 
 gitblame.init(function(res) {
-   console.log('The line that caused your error : \n' + res.blame + '\n');
+   console.log('The person that caused your error : \n' + res.blame + '\n');
+   console.log('The email address of that user : \n' + res.email + '\n');
    console.log('The error that caused the uncaught : \n' + res.error + '\n');
    console.log('The file that caused the error: \n' + res.filename + '\n');
    console.log('The line in the file that caused your error: \n' + res.line + '\n');
+   console.log('The original stack that caused the error: \n');
+   console.dir(res.stack);
 });
 
 
--- tattletales.js
+++ tattletales.js
@@ -3,39 +3,62 @@ var path = require('path');
 
 module.exports = {
   init: function(callback) {
-     var that = this;
-     callback = callback || function (res) { console.log(res); };
-     (function (fn) {
-	process.on('uncaughtException', function(err) {
-	   that.blame(err.stack, function (err, res) {
-	      if (err) return
-	      fn(res);
-           });
+    var that = this;
+    callback = callback || function (res) { console.log(res); };
+    (function (fn) {
+    	process.on('uncaughtException', function(err) {
+    	  that.blame(err.stack, function (err, res) {
+    	    if (err) return;
+    	    fn(res);
         });
-     })(callback);
+      });
+    })(callback);
   },
   blame: function (stack, callback) {
-     var stack = stack.split(/\n/),
-	 error = null,
-	 line = null,
-	 filename = null;
+    var stack = stack.split(/\n/),
+  	 error = null,
+  	 line = null,
+  	 filename = null;
     
-     if (stack.length < 2) callback(err);	
+    if (stack.length < 2) callback(err);	
 
-     error = stack[0];
+    error = stack[0];
 	
-     line = /\(.*\)$/.exec(stack[1]);
-     line = line[0];
-     line = line.substring(1, line.length-1);
-     line = line.split(':');
+    line = /\(.*\)$/.exec(stack[1]);
+    line = line[0];
+    line = line.substring(1, line.length-1);
+    line = line.split(':');
     
-     filename = line[0]; 
-     exec('git blame ' + filename + ' -L' + line[1] + ',+1', function (err, stdout, stderr) {
-     	if (err !== null) {
-       	   return callbackb(new Error(err));
-      	}
-	var response = stdout.replace(/(\r\n|\n|\r)/gm,"");
-      	callback(null, {blame: response, error: error, filename: filename, line: line[1]});
-     });
+    filename = line[0]; 
+    exec('git blame ' + filename + ' --line-porcelain -L' + line[1] + ',+1', function (err, stdout, stderr) {
+      if (err !== null) {
+        return callback(err);
+      }
+      var response, 
+          author,
+          email;
+
+	    response = stdout.split("\n");
+      author = response[1].replace("author ", "");
+      email = response[2].replace("author-mail", "").replace("<", "").replace(">", "").trim();
+
+      callback(null, {
+        blame: author, 
+        email: email, 
+        error: error, 
+        filename: filename, 
+        line: line[1], 
+        stack: stack
+      });
+    });
+  },
+  primary_caretaker: function (file, callback) {
+    exec("git blame --line-porcelain "  + file + " | sed -n 's/^author-mail //p' | sort | uniq -c | sort -rn", function (err, stdout, stderr) {
+      if (err !== null) {
+        return callback(err);
+      }
+      var response = stdout.replace(/(\r\n|\n|\r)/gm,"");
+      console.log(response);
+    });
   }
 };
